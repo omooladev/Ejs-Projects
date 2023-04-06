57a8dadd617b575b63aaad513fb45c24aa563f6c
omoola <omooladev@gmail.com>
2023-04-06 05:56:52 -0700

    working on the outputting the grocery item

--- grocery-bud/controllers/Groceries.js
+++ grocery-bud/controllers/Groceries.js
@@ -1,6 +1,19 @@
-
+const dummyData = [
+  {
+    id: 1,
+    itemName: "egg",
+  },
+  {
+    id: 2,
+    itemName: "fish",
+  },
+  {
+    id: 3,
+    itemName: "donut",
+  },
+];
 const getGroceries = (req, res) => {
-  res.render("index.ejs", { pageTitle: "Groceries" });
+  res.render("index.ejs", { pageTitle: "Groceries", groceryItems: dummyData });
 };
 
 module.exports = { getGroceries };
