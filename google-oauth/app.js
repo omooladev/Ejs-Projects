962f5578ded810308a2515af717e84b7a336a40e
omodev <omooladev@gmail.com>
2023-04-23 20:01:37 +0100

    working on the auth routes

--- google-oauth/app.js
+++ google-oauth/app.js
@@ -1,1 +1,11 @@
-console.log("app is running")
+const express = require("express");
+
+const app = express();
+
+app.set("view engine", ejs);
+
+const startServer = async () => {
+  try {
+  } catch (error) {}
+};
+startServer();
