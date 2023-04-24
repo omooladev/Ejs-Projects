7ddbf92d23ec8c12915ecaa01447413eff4896e7
omodev <omooladev@gmail.com>
2023-04-24 16:01:37 +0100

    added user to the deader

--- google-oauth/routes/User.js
+++ google-oauth/routes/User.js
@@ -4,10 +4,10 @@ const router = express.Router();
 
 //* routes
 router.route("/").get((req, res) => {
-  res.render("Home.ejs", { pageTitle: "Home Page" });
+  res.render("Home.ejs", { pageTitle: "Home Page", user: null });
 });
 router.route("/profile").get((req, res) => {
-  res.render("Profile.ejs", { pageTitle: "Profile Page" });
+  res.render("Profile.ejs", { pageTitle: "Profile Page", user: null });
 });
 
 module.exports = router;
