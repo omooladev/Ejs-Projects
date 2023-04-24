0ee9e7595bbc357ece4da3077e09dc427a1a4947
omodev <omooladev@gmail.com>
2023-04-24 18:01:24 +0100

    refactored code

--- google-oauth/app.js
+++ google-oauth/app.js
@@ -4,6 +4,11 @@ const userRoutes = require("./routes/User");
 const NotFound = require("./middlewares/not-found");
 const app = express();
 
+// GOOGLE_CLIENT_ID = "676203263085-6jglegmkpti3eqp3qjr1dp11acag5lqf.apps.googleusercontent.com";
+// GOOGLE_CLIENT_SECRET = "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";
+// GOOGLE_CLIENT_REDIRECT_URL = "http://localhost:5000/auth/google/redirect";
+
+
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
---
+++ google-oauth/config/passport-setup.js
@@ -0,0 +1,5 @@
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+
+
+
+
