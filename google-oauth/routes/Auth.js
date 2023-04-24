4a3e00d2bcefb1841c7d7be0b3eb0fe389c9f4cf
omodev <omooladev@gmail.com>
2023-04-24 17:02:53 +0100

    working on the google route

--- google-oauth/routes/Auth.js
+++ google-oauth/routes/Auth.js
@@ -3,7 +3,8 @@ const express = require("express");
 const router = express.Router();
 
 //* routes
-router.route("/").get((req, res) => {
-  res.render("Home");
+router.route("/login").get((req, res) => {
+  res.render("Login.ejs", { pageTitle: "Login", user: null });
 });
+router.route("/login/google").get((req, res) => {});
 module.exports = router;
