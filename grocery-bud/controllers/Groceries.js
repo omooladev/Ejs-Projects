4a4928a1f88693ca51ec93f566e9978197a99ea3
omodev <omooladev@gmail.com>
2023-04-28 22:44:35 +0100

    saved grocery items to local storage

--- grocery-bud/controllers/Groceries.js
+++ grocery-bud/controllers/Groceries.js
@@ -1,19 +1,5 @@
 const { BadRequestError } = require("../errors/bad-request");
-
-const dummyData = [
-  {
-    id: 1,
-    itemName: "Egg",
-  },
-  {
-    id: 2,
-    itemName: "fish",
-  },
-  {
-    id: 3,
-    itemName: "donut",
-  },
-];
+const { StatusCodes } = require("http-status-codes");
 const getGroceries = (req, res) => {
   res.render("index.ejs", { pageTitle: "Groceries", groceryItems: null });
 };
@@ -26,9 +12,7 @@ const createGrocery = (req, res) => {
   }
   const newItem = { id: Math.random(), itemName };
 
-  dummyData.push(newItem);
-
-  // res.render("index.ejs", { pageTitle: "Groceries", groceryItems: dummyData });
+  res.status(StatusCodes.CREATED).json({ groceryItem: newItem });
 };
 
 module.exports = { getGroceries, createGrocery };
--- grocery-bud/public/scripts/index.js
+++ grocery-bud/public/scripts/index.js
@@ -1,6 +1,25 @@
 const form = document.querySelector("form");
 const groceryInput = document.querySelector("#grocery_input");
 const reply = document.querySelector(".reply");
+const getGroceryItemsFromLocalStorage = async () => {
+  //? get grocery items from local storage
+  let groceryItems = localStorage.getItem("groceryItems");
+  if (!groceryItems) {
+    return null;
+  }
+  return JSON.parse(groceryItems);
+};
+const saveGroceryItemToLocalStorage = async (groceryItem) => {
+  //? get grocery items from local storage
+  let groceryItems = await getGroceryItemsFromLocalStorage();
+  if (!groceryItems) {
+    groceryItems = JSON.stringify([groceryItem]);
+  } else {
+    groceryItems = JSON.stringify([groceryItem, ...groceryItems]);
+  }
+
+  return localStorage.setItem("groceryItems", groceryItems);
+};
 const submitFormHandler = async (event) => {
   event.preventDefault();
   let groceryValue = groceryInput.value;
@@ -11,7 +30,10 @@ const submitFormHandler = async (event) => {
   }
   groceryValue = { itemName: groceryValue };
   try {
-    const response = await axios.post("/groceries", groceryValue);
+    const {
+      data: { groceryItem },
+    } = await axios.post("/groceries", groceryValue);
+    await saveGroceryItemToLocalStorage(groceryItem);
   } catch (err) {
     const response = err.response || "";
     const data = response.data || "";
