b6e1229f11d1601b1b25b1d1a207f86b698eec87
omodev <omooladev@gmail.com>
2023-05-05 17:43:33 +0100

    working on the scripts file

--- oauth/public/script.js
+++ oauth/public/script.js
@@ -1,7 +1,19 @@
-const EyeContainer = document.querySelector(".eye_container");
+
 const passwordInput = document.querySelector(".password_container input");
-const reply = document.querySelector(".reply");
-console.log(reply);
+const authReply = document.querySelector(".reply");
+
+//? ------------------------------------------------- //
+
+
+const authUsername = document.querySelector(".auth_username_input");
+const authEmailAddress = document.querySelector(".auth_email_address_input");
+const authPassword = document.querySelector(".auth_password_input");
+const authFormButton = document.querySelector(".auth_form .form_actions button");
+
+const submitFormHandler = async (event) => {
+  console.log(authFormButton.className);
+};
+authFormButton.addEventListener("click", submitFormHandler);
 
 EyeContainer.addEventListener("click", () => {
   EyeContainer.classList.toggle("show");
