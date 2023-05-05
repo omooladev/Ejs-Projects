d3a9226af9ff6ec635003638206d9f740590f861
omodev <omooladev@gmail.com>
2023-05-05 22:23:07 +0100

    refactored code

--- oauth/app.js
+++ oauth/app.js
@@ -12,7 +12,7 @@ const app = express();
 // "engines": {
 //     "node": ">=14 <15"
 //   }
-app.set("views", path.join(__dirname,"../views"));
+// app.set("views", path.join(__dirname,"../views"));
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(
