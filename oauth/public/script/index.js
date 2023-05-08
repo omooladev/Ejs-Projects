451790ae7762844524917a261c020ff986b5015b
omodev <omooladev@gmail.com>
2023-05-08 20:05:18 +0100

    created another javascript file in the script

--- oauth/public/script/index.js
+++ oauth/public/script/index.js
@@ -12,26 +12,56 @@ const authFormButton = document.querySelector(".auth_form .form_actions button")
 /*? ------------------------------------------------ */
 /**              //Functions                         */
 /*? ------------------------------------------------ */
+
+const sendRequest = async ({ method = "GET", uri, userData = null }) => {
+  console.log(uri, userData);
+};
+
 const validateForm = async (formTypeIsLogin, formTypeIsSignUp) => {
   let authUsernameValue;
   let authEmailAddressValue;
   let authPasswordValue = authPassword.value;
-  let checkGlobal;
   try {
     if (formTypeIsLogin) {
       authUsernameValue = authUsername.value;
-      if (!authUsernameValue && !authPasswordValue)
+      if (!authUsernameValue && !authPasswordValue) {
         throw new Error("Please provide your credentials");
-      if (!authUsernameValue) throw new Error("Please provide your username");
+      }
+      if (!authUsernameValue) {
+        authUsername.focus();
+        throw new Error("Please provide your username");
+      }
+      if (authUsernameValue && authPasswordValue) {
+        const response = await sendRequest({
+          method: "POST",
+          uri: "/auth/login",
+          userData: { username: authUsernameValue, password: authPasswordValue },
+        });
+        return;
+      }
     }
     if (formTypeIsSignUp) {
       authEmailAddressValue = authEmailAddress.value;
-      if (!authEmailAddressValue && !authPasswordValue)
-        throw new Error("Please provide your credentials");
-      if (!authEmailAddressValue) throw new Error("Please provide your email address");
+      if (!authEmailAddressValue && !authPasswordValue) {
+        throw new Error("Please provide your email address and password");
+      }
+      if (!authEmailAddressValue) {
+        authEmailAddress.focus();
+        throw new Error("Please provide your email address");
+      }
+      if (authEmailAddressValue && authPasswordValue) {
+        const response = await sendRequest({
+          method: "POST",
+          uri: "/auth/signup",
+          userData: { emailAddress: authEmailAddressValue, password: authPasswordValue },
+        });
+        return;
+      }
     }
-    if (!authPasswordValue) throw new Error("Please provide your password");
-    if (checkGlobal) console.log(formTypeIsLogin, formTypeIsSignUp);
+    if (!authPasswordValue) {
+      authPassword.focus();
+      throw new Error("Please provide your password");
+    }
   } catch (error) {
     authReply.innerHTML = error.message;
     authReply.classList.add("error");
